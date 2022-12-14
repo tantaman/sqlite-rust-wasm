a6b1956d3147b1cf967dba777c27b07c213acf36
Matt <1009003+tantaman@users.noreply.github.com>
2022-12-14 16:29:27 -0500

    example app to load and use the rust linked wasm

---
+++ js/example/src/main.ts
@@ -0,0 +1,30 @@
+import * as SQLite from "wa-sqlite";
+import SQLiteAsyncESMFactory from "wa-sqlite/dist/wa-sqlite-async.mjs";
+// @ts-ignore
+import { IDBBatchAtomicVFS } from "wa-sqlite/src/examples/IDBBatchAtomicVFS.js";
+// @ts-ignore
+import wasmUrl from "wa-sqlite/dist/wa-sqlite-async.wasm?url";
+import { tag } from "./tag";
+
+const module = await SQLiteAsyncESMFactory({
+  locateFile(file: string) {
+    return wasmUrl;
+  },
+});
+const sqlite3 = SQLite.Factory(module);
+sqlite3.vfs_register(
+  new IDBBatchAtomicVFS("idb-batch-atomic", { durability: "relaxed" })
+);
+
+const filename = null;
+const db = await sqlite3.open_v2(
+  filename || ":memory:",
+  SQLite.SQLITE_OPEN_CREATE |
+    SQLite.SQLITE_OPEN_READWRITE |
+    SQLite.SQLITE_OPEN_URI,
+  filename != null ? "idb-batch-atomic" : undefined
+);
+
+const sql = tag(sqlite3, db);
+
+console.log(await sql`SELECT * FROM sqlite_master`);
--- rs/test_extension/src/lib.h
+++
@@ -1,6 +0,0 @@
-#ifndef TEST_EXTENSIONS_H
-#define TEST_EXTENSIONS_H
-
-int testext_commit_hook(void *pUserData);
-
-#endif
